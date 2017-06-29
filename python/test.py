f64859f05a0b026df2eac92de2dabe85033e6858
Vikum94 <vikumbandara@hotmail.com>
2017-06-29 16:55:35 +0800

    Test.py MQTT working. Checked with mqtt-spy

--- python/test.py
+++ python/test.py
@@ -4,22 +4,17 @@ import time
 host="iot.eclipse.org"
 msg = ""
 
-def on_message(client, userdata, message):
-	print("message: ", str(message.payload.decode("utf-8")))
-	print("topic: ",message.topic)
-
 client = mqtt.Client()
-
-client.on_message = on_message
 client.connect(host, 1883, 60)
 
 print("Publishing")
 msg="Work"
-client.publish("sutd/2.601", msg, 2)
+client.publish("sutd/2.601", "Working")
 print("###Published###")
 
-print("Pulbishing again")
-msg="No Work"
-client.publish("sutd/2.601", msg, 2)
-print("###Published###")
+#print("Pulbishing again")
+#msg="No Work"
+#client.publish("/sutd/2.601", msg, 2)
+#print("###Published###")
 
+client.disconnect()
